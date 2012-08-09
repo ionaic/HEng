56dc9c75a85f833a8ddffdb2f9f1780d64a7ddc1
Ian Ooi <ionaic@gmail.com>
2012-08-09 16:03:36 -0400

    added more doc to help Matt

--- src/firstGlfw.hs
+++ src/firstGlfw.hs
@@ -14,7 +14,24 @@ rendLoop = do
         -- IO Bool, which is really just a Bool in an IO monad....
         --
         -- UPDATE: the windowCloseCallback I belive just needs to be a flag
-        --   of close or no
+        --   of close or no.  return is a function which puts something into
+        --   a monad, and >> just sticks two monads together and gives you a 
+        --   monad containing the second arg, so as far as i can tell, it ends
+        --   up being a monad (IO monad specifically) containing the IORef and
+        --   the returned value, which we could read as such:
+        --
+        --          writeIoRef quit True >>= return readIORef quit
+        --
+        --   but that's rather pointless, since we just set quit to True, and
+        --   therefore know its value already.  the existing code therefore
+        --   writes to the IORef true and returns, what i'm guessing is a success
+        --   status.
+        --
+        --   alternatively could use the following as callback:
+        --          do
+        --              GLFW.closeWindow
+        --              GLFW.terminate
+        --              return True
         GLFW.windowCloseCallback $= (writeIORef quit True >> return True)
 
         -- start main loop and define it using where clause
