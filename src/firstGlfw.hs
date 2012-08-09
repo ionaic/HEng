4750138fdcd01fa8487832f13c5c3b974a07a7c1
Ian Ooi <ionaic@gmail.com>
2012-08-09 15:24:04 -0400

    made some progress on first haskell glfw program

---
+++ src/firstGlfw.hs
@@ -0,0 +1,34 @@
+import Graphics.UI.GLFW as GLFW
+import Graphics.Rendering.OpenGL as GL
+import Data.IORef
+import Control.Monad
+import System.Environment
+
+rendLoop = do 
+        -- set up a flag to check if the window is closed
+        quit <- newIORef False
+ 
+        -- set up the window close callback function (i'm a little shaky here)
+        -- i believe it's just a flag and it lets you stop the program if 
+        -- quit is true, because type of the RHS of the equation is an
+        -- IO Bool, which is really just a Bool in an IO monad....
+        GLFW.windowCloseCallback $= (writeIORef quit True >> return True)
+
+        -- start main loop and define it using where clause
+        loop quit where
+            loop quit = do
+                q <- readIORef quit
+                unless q $ loop quit
+
+main = do
+    -- initialize GLFW
+    GLFW.initialize
+
+    -- open a window 
+    GLFW.openWindow (GL.Size 300 300) [GLFW.DisplayAlphaBits 8] GLFW.Window
+    -- set window title
+    GLFW.windowTitle $= "butts"
+    GL.clearColor $= Color4 0 0 0 0
+    rendLoop
+    GLFW.closeWindow
+    GLFW.terminate
