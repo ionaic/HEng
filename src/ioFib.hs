bdd0360416363bca7d3325438ed73e8ea89c6ca1
Ian Ooi <ionaic@gmail.com>
2012-08-09 02:10:35 -0400

    basic fibonacci sequence with IO for Matt

---
+++ src/ioFib.hs
@@ -0,0 +1,10 @@
+import System.Environment
+
+fib :: Int -> Int
+fib 1 = 1
+fib 2 = 1
+fib num = (fib $ num - 1) + (fib $ num - 2)
+
+main = do 
+        [f] <- getArgs
+        putStrLn $ show $ (fib (read f::Int))
