87ad0efc41c8ce11cc11f7108e1f5dcaf305728e
Ian Ooi <ionaic@gmail.com>
2012-08-13 02:17:35 -0400

    I have no idea what I m doing, maybe Matt can take a look

--- example/do-lessfirstGLFW.hs
+++ example/do-lessfirstGLFW.hs
@@ -3,38 +3,20 @@ import Graphics.Rendering.OpenGL as GL
 import Data.IORef
 import Control.Monad
 
--- data Resources = Resources {vertexArrayVBO :: BufferObject,
---                            elementBufferVBO :: BufferObject}
-                    
+--initGeometry = 
 
--- function which simply returns the GLfloat array of raw vertex data
-rawVertexData :: [GLfloat]
-rawVertexData = [-1.0, 0.0, 0.0,  0.0, 1.0, 0.0,  0.0, 0.0, 0.0]
-
--- function which simply returns the GLfloat array of raw color data
-rawColorData :: [GLfloat]
-rawColorData = [1.0, 0.0, 0.0,  0.0, 1.0, 0.0,  0.0, 0.0, 1.0]
-
---initGeometry = bindBuffer ArrayBuffer (makeStateVar Resources vertexArrayVBO)
-initGeometry = genObjectNames 1 >>= \x@(vbo:t) -> bindBuffer ArrayBuffer $= Just vbo
-        
-
-
---Resources (bindBuffer ArrayBuffer rawVertexData) (bindBuffer 
-
+-- render = do
 render = GL.clear [GL.ColorBuffer, GL.DepthBuffer]
         
 
-rendMain :: IO Bool
+--rendMain = do 
 rendMain =
-        -- set a flag to check if the window is closed
+        -- set up a flag to check if the window is closed
+        --quit <- newIORef False
         newIORef False >>= \quit -> 
 
         -- set up the window close callback function
-        GLFW.windowCloseCallback $= (writeIORef quit True >> 
-                                GLFW.closeWindow >> 
-                                GLFW.terminate >> 
-                                return True) >>
+        GLFW.windowCloseCallback $= (writeIORef quit True >> GLFW.closeWindow >> GLFW.terminate >> return True) >>
 
         -- Set the clear color
         GL.clearColor $= Color4 0 0 0 0 >>
@@ -49,6 +31,7 @@ rendMain =
         -- start main loop and define it using where clause
         loop quit
         where
+            -- loop quit = do
             loop quit =
                 -- Tells GLFW to look for event callbacks
                 GLFW.waitEvents >>
@@ -57,11 +40,15 @@ rendMain =
                 render >> GLFW.swapBuffers >>
 
                 -- read if quit has been called or not
-                -- if quit is false, continue loop.  otherwise we're done!
+                -- q <- readIORef quit
                 readIORef quit >>= \q -> case q of 
                     False -> loop quit
                     _ -> return True
+                -- unless quit is true, continue loop.  otherwise we're done!
+                -- unless q $ loop quit
+                -- unless readIORef quit loop quit
 
+-- main = do
 main =
     -- initialize GLFW
     GLFW.initialize >>
